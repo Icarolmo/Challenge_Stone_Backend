8fc2db7bfdcbd96f9dde89aa2f08916ed6617aff
Icarus <icarolmol@gmail.com>
2023-10-25 21:23:55 -0300

    Fix: initial files project.

---
+++ src/test/java/com/stone/challenge/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stone.challenge.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
